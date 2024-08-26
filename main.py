68bdf727f756efec915f43bc09a456d1bc8e5e51
Michael Bironneau <michael.bironneau@learnd.co.uk>
2024-08-26 15:46:41 +0100

    Make server configurable and clean up pyinstaller configs

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-
+import argparse
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 import multiprocessing
 from faster_whisper import WhisperModel
 
+# Argument parser to handle command-line arguments
+parser = argparse.ArgumentParser(description="Whisper server")
+
+parser.add_argument('--port', type=int, default=8000, help="Port to run the server on")
+parser.add_argument('--model_dir', type=str, default="./model", help="Path to the model directory")
+parser.add_argument('--device', type=str, default="cpu", help="Device to run the model on (e.g., 'cpu', 'cuda')")
+
+args = parser.parse_args()
+
+# Initialize the FastAPI app and model with command-line arguments
 app = FastAPI()
-model = WhisperModel("./model", device="cpu", compute_type="int8")
+model = WhisperModel(args.model_dir, device=args.device, compute_type="int8")
 
 @app.post("/segment")
 async def process_sample(file: Annotated[UploadFile, File()]):
@@ -24,4 +34,4 @@ async def process_sample(file: Annotated[UploadFile, File()]):
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()  # For Windows support
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False, workers=1)
+    uvicorn.run(app, host="127.0.0.1", port=args.port, reload=False, workers=1)
--- main.spec
+++ main.spec
@@ -22,7 +22,7 @@ exe = EXE(
     a.binaries,
     a.datas,
     [],
-    name='main',
+    name='whisper-server',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
